c246234eb836a707b9965171b79e9fea91a7ec82
CoetzeeNP <coetzeenp@ufs.ac.za>
2026-02-18 11:05:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from database import save_to_firebase, update_previous_feedback
 from streamlit_cookies_controller import CookieController
 
 # Setup & Configuration
-st.set_page_config(layout="wide", page_title="AI-frikaans Assistant")
+st.set_page_config(layout="wide", page_title="ThunderbAIrd Assistant")
 #controller = CookieController()
 
 # Custom CSS
